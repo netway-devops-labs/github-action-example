1e4ec317121d621c013d7d19cebe49beeb752455
Nattawut <nattawut@netway.co.th>
2020-10-06 15:03:10 +0700

    edit index html

--- public/index.php
+++ public/index.php
@@ -1,4 +1,22 @@
+<!DOCTYPE html>
+
 <html>
-hello world
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <title>Spoon-Knife</title>
+    <LINK href="styles.css" rel="stylesheet" type="text/css">
+</head>
+
+<body>
+
+    <img src="forkit.gif" id="octocat" alt="" />
+
+    <!-- Feel free to change this text here -->
+    <p>
+        Fork me? Fork you, @octocat!
+    </p>
+
+</body>
 
 </html>
